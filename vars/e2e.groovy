fa3a94d64915b47250e2ab3d3be9ed38f9755354
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2021-03-09 17:58:15 +0530

    Update e2e.groovy

--- vars/e2e.groovy
+++ vars/e2e.groovy
@@ -15,9 +15,9 @@ pipeline {
         // This can be http or https
         NEXUS_PROTOCOL = "http"
         // Where your Nexus is running. 'nexus-3' is defined in the docker-compose file
-        NEXUS_URL = "http://13.127.242.86:8081/nexus"
+        NEXUS_URL = "http://13.127.242.86:8081"
         // Repository where we will upload the artifact
-        NEXUS_REPOSITORY = "maven"
+        NEXUS_REPOSITORY = "maven-repo"
         // Jenkins credential id to authenticate to Nexus OSS
         NEXUS_CREDENTIAL_ID = "nexus-credentials"
     }
