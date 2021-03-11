b7bbacb984a56e9fbb0651ebf0b39dd86c6dbecc
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2021-03-11 12:08:36 +0530

    Create nexus_test_new.groovy

---
+++ vars/nexus_test_new.groovy
@@ -0,0 +1,27 @@
+def call( ) {
+    
+   
+                  def  pom = readMavenPom file: "pom.xml";
+                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                    artifactPath = filesByGlob[0].path;
+                    artifactExists = fileExists artifactPath;
+                    if(artifactExists) {
+                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+                 
+            
+    }
+
+  nexusupload([
+   $class: 'NexusArtifactUploader',
+   artifacts: [[artifactId: pom.artifactId, classifier: '', file: artifactPath, type: pom.packaging]], 
+   credentialsId: NEXUS_CREDENTIAL_ID, 
+   groupId: pom.groupId, 
+   nexusUrl: NEXUS_URL, 
+   nexusVersion: 'nexus3', 
+   protocol: NEXUS_PROTOCOL, 
+   repository: NEXUS_REPOSITORY,
+   version: pom.version
+   ])
+                    
+    }
