dbee39475bb3e3aa10a84ff0c289d5e80ba06a58
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2021-04-08 17:26:51 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 def call(String Jname) {
  
    
- sh "scp -r /root/.jenkins/workspace/$Jname/target/*.war root@52.152.229.140:/root/tool/apache-tomcat-8.5.64/webapps/"
+ sh "scp -o StrictHostKeyChecking=no -r /root/.jenkins/workspace/$Jname/target/*.war root@52.152.229.140:/root/tool/apache-tomcat-8.5.64/webapps/"
   
 }
