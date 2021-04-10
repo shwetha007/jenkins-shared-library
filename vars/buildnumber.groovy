a5338812134d6ab0ea983a68bf75886434d332dc
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2021-04-10 11:18:27 +0530

    Create buildnumber.groovy

---
+++ vars/buildnumber.groovy
@@ -0,0 +1,4 @@
+def call() {
+  int Bnumber = sh(script: "curl -u $JFROG_ID -s $url | grep uri |awk '{print $3}'| sed 's+\"++g' | sed 's+/++g' | sed 's+,++g' |wc -l",returnStdout: true).trim()
+  return Bnumber
+}
