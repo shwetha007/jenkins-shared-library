dc1068ce4d57c407d89032668963ab4c7ea82e1d
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2021-04-10 11:55:18 +0530

    Create curlmethod.groovy

---
+++ vars/curlmethod.groovy
@@ -0,0 +1,15 @@
+def call(String url, String JFROG_ID,int bn ) {
+
+//String resp = sh(script: "curl -u $JFROG_ID -s $url | grep uri | awk '{ print \$3 }' | sed 's+\"++g' | sed 's+/++g' | sed 's+,++g' | head -1", returnStdout: true).trim()
+
+//return resp
+ String lt
+    if(bn > 1){
+        lt = sh(script: "curl -u $JFROG_ID -s $url | grep uri |awk '{print \$3}'| sed 's+\"++g' | sed 's+/++g' | sed 's+,++g' | head -$bn | tail -1", returnStdout: true).trim();
+    }else{
+        lt = sh(script: "curl -u $JFROG_ID -s $url | grep uri |awk '{print \$3}'| sed 's+\"++g' | sed 's+/++g' | sed 's+,++g' | head -$bn", returnStdout: true).trim();
+    }
+    
+    return lt
+
+}
