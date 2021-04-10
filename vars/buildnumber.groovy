9358b1e1cf6cb07c9f5bab00a2297652979f702e
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2021-04-10 11:24:52 +0530

    Update buildnumber.groovy

--- vars/buildnumber.groovy
+++ vars/buildnumber.groovy
@@ -1,4 +1,5 @@
 def call() {
+  String url = "https://jfrgfreetst.jfrog.io/artifactory/api/storage/example-repo-local/?sort"
   int Bnumber = sh(script: "curl -u $JFROG_ID -s $url | grep uri |awk '{print \$3}'| sed 's+\"++g' | sed 's+/++g' | sed 's+,++g' |wc -l",returnStdout: true).trim()
   return Bnumber
 }
