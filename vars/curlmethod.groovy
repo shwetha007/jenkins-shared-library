0577729ef1e4b2c53bb1a82f9d76fe8bc761bb67
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2021-04-13 08:33:54 +0530

    Update curlmethod.groovy

--- vars/curlmethod.groovy
+++ vars/curlmethod.groovy
@@ -5,9 +5,9 @@ def call(String url, String JFROG_ID,String bn ) {
 //return resp
  String lt
     if(bn > 1){
-        lt = sh(script: "curl -u $JFROG_ID -s $url | grep uri |awk '{print \$3}'| sed 's+\"++g' | sed 's+/++g' | sed 's+,++g' | head -$bn | tail -1", returnStdout: true).trim();
+        lt = sh(script: "curl -u $JFROG_ID -s $url | grep uri |awk '{print \$3}'| sed 's+\"++g' | sed 's+/++g' | sed 's+,++g' |sed 's+'https:jfrgfreetst.jfrog.ioartifactoryapistorageexample-repo-local'++g' | sed 's+'Mypath2'++' |head -c -2 | head -$bn | tail -1", returnStdout: true).trim();
     }else{
-        lt = sh(script: "curl -u $JFROG_ID -s $url | grep uri |awk '{print \$3}'| sed 's+\"++g' | sed 's+/++g' | sed 's+,++g' | head -$bn", returnStdout: true).trim();
+        lt = sh(script: "curl -u $JFROG_ID -s $url | grep uri |awk '{print \$3}'| sed 's+\"++g' | sed 's+/++g' | sed 's+,++g' |sed 's+'https:jfrgfreetst.jfrog.ioartifactoryapistorageexample-repo-local'++g' | sed 's+'Mypath2'++' |head -c -2 | head -$bn", returnStdout: true).trim();
     }
     
     return lt
