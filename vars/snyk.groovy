10ed7053c4e44439a5a1b557b4389599e31cf45d
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2021-10-28 15:28:03 +0530

    Update snyk.groovy

--- vars/snyk.groovy
+++ vars/snyk.groovy
@@ -1,3 +1,4 @@
 def call() {
-  snykSecurity additionalArguments: '--docker myimage:12', failOnIssues: false, snykInstallation: 'mySnyk', snykTokenId: 'mySnkToken'
+  //snykSecurity additionalArguments: '--docker myimage:12', failOnIssues: false, snykInstallation: 'mySnyk', snykTokenId: 'mySnkToken'
+  echo "hi Prasad"
 }
