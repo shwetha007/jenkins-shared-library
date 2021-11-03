9c4d9f2bc41a6cc921d5be73dd2533147c0b9456
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2021-11-03 16:39:26 +0530

    Update snyka.groovy

--- vars/snyka.groovy
+++ vars/snyka.groovy
@@ -1,4 +1,4 @@
-def call(int buildnumber) {
+def call(String buildnumber) {
   snykSecurity additionalArguments: '--docker myimage:$buildnumber', failOnIssues: false, snykInstallation: 'mySnyk', snykTokenId: 'mySnkToken'
   echo "hi Prasad"
 }
